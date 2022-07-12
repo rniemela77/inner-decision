1f1d8bedcb5592fa35592d37e6dbf4b84a30e132
Robert Niemela <rvniemela@hotmail.com>
2022-07-11 17:45:12 -0700

    refactor: put sample goals on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <h1>Goals</h1>
+                    <ul>
+                        <li>Task 1 - EASY</li>
+                        <li>Task 2 - EASY</li>
+                        <li>Task 3 - EASY</li>
+                    </ul>
+                    
+                    <h1>Duke's Goals</h1>
+                    <ul>
+                        <li>Task 1</li>
+                        <li>Task 2</li>
+                        <li>Task 3</li>
+                    </ul>
                 </div>
             </div>
         </div>
