bfdc914db457f9b8b77568d3d18abb5bcdbcd20b
Robert Niemela <rvniemela@hotmail.com>
2022-07-13 19:39:29 -0700

    feat(gp): use foreach to render all goals

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Goal;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        'goals' => Goal::all()
+    ]);
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
